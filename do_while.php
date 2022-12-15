0a237e14b75d6c1678c51300f1993e76ff03afbf
Ichika354 <handa82624@gmail.com>
2022-12-15 10:47:20 +0700

    Tugas PHP

---
+++ do_while.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Do While</title>
+</head>
+<body>
+    <h2>Penggunaan kontrol do while</h2>
+    Kelipatan 3 Adalah
+    <?php 
+     $i = 0;
+     $var = 0;
+
+     do {
+        $var + 3;
+        echo ("$var, ");
+        $i++;
+     } while ($i <= 4) 
+    
+    ?>
+
+
+
+</body>
+</html>
